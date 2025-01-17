735db3e482df597fe2de1c7fff825d01186625ce
khanloo <m.nadirkhanloo1380@gmail.com>
2025-01-17 16:31:54 +0330

    Feat : add link active

---
+++ src/components/NavLink.jsx
@@ -0,0 +1,30 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { Link as MuiLink } from "@mui/material";
+import { gray, shades } from "@/theme/color";
+
+function NavLink({ path, children }) {
+  const pathname = usePathname();
+  return (
+    <MuiLink
+      component={Link}
+      href={path}
+      sx={{
+        color: pathname === path ? shades.primary : gray[7],
+        textDecoration: pathname === path ? "underline" : "none",
+        textDecorationColor: shades.primary,
+        textUnderlineOffset: "10px",
+        fontWeight: pathname === path ? 700 : 500,
+        transition: "all 0.5s ease",
+        width:"100%",
+        "&:hover": {
+          color: shades.primary,
+        },
+      }}
+    >
+      {children}
+    </MuiLink>
+  );
+}
+export default NavLink;
